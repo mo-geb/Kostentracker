391299319444114726a290dd2a62e68d48bd3b45
Moritz Gebhardt <moritzg.online@gmail.com>
2025-06-27 11:39:12 +0200

    Initial Commit

---
+++ Kostentracker/KostentrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KostentrackerApp.swift
+//  Kostentracker
+//
+//  Created by Moritz Gebhardt on 27.06.25.
+//
+
+import SwiftUI
+
+@main
+struct KostentrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KostentrackerTests/KostentrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  KostentrackerTests.swift
+//  KostentrackerTests
+//
+//  Created by Moritz Gebhardt on 27.06.25.
+//
+
+import Testing
+
+struct KostentrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
